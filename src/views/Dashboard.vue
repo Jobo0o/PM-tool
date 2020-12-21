8bafa48bcde09cb3c1bec3cb5eddbfea0f1d67c0
JOHANNES SCHENK <Johannes.schenk2@gmx.de>
2020-12-21 16:06:33 +0100

    navbar, drawer, title, router & content

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashabord">
+    <h1 class="subtitle-3 grey--text">Dashboard</h1>
+
+    <v-container class="my-5">
+        content
+    </v-container>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+
+}
+</script>
