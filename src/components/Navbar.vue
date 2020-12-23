d372ba77b8b52b9dc548f94e9d17fd1047b2d2d2
JOHANNES SCHENK <Johannes.schenk2@gmx.de>
2020-12-23 15:10:43 +0100

    Dashboard Update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,7 @@
             </v-btn>
         </v-app-bar>
 
-        <v-navigation-drawer app class="primary" v-model="drawer">
+        <v-navigation-drawer dark app class="primary" v-model="drawer">
             <v-list class="pa-4">
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
